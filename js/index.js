1069d95e017e5bb380202ceb6e4dfb5bff13bac3
SG <stephsstar@gmail.com>
2019-12-03 23:00:31 +0100

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//Responsive Navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+//Copyright Date Function
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
