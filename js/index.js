d1fbee7632576f702388d87fd74dfff70966e72a
SG <stephsstar@gmail.com>
2019-12-13 01:44:07 +0100

    + Nordlys; fixed meetup thumbnail; added js

--- js/index.js
+++ js/index.js
@@ -12,4 +12,23 @@ function myFunction() {
 //Copyright Date Function
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
+//Meetup case study scroll bar (originally from w3 schools)
+window.onscroll = function() {myScrollBar()};
 
+function myScrollBar() {
+ var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+ var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+ document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//Change background color of Northern Lights img.
+function myPicture() { 
+    var e = document.getElementById("demo");
+    var c = window.getComputedStyle(e).backgroundColor;
+    if (c === "rgb(255, 255, 255)") {
+    document.getElementById("demo").style.background = "#000";
+} else{
+    document.getElementById("demo").style.background = "#FFF";
+}
+}
